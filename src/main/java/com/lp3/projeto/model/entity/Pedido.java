472f1716720545645a4c0b736b7c4330da7bfb5b
Mauricio Azevedo Franco Ladeira <mauricioafl001@gmail.com>
2022-04-25 20:07:46 -0300

    add classe: pedido

--- src/main/java/com/lp3/projeto/model/entity/Pedido.java
+++ src/main/java/com/lp3/projeto/model/entity/Pedido.java
@@ -1,28 +1,28 @@
 package com.lp3.projeto.model.entity;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.Date;
 
+
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public abstract class Cliente {
+public class Pedido {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String telefone;
-    private String email;
-    private String nome;
-    private String senha;
+    private Date dataDoPedido;
 
-    @OneToMany
-    private Endereco endereco;
+    @ManyToOne
+    private PessoaFisica pf;
 
-
+    @ManyToOne
+    private PessoaJuridica pj;
 }
