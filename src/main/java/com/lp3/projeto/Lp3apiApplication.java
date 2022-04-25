9a883c33079c5909336e603d04b9f293469e2111
Mauricio Azevedo Franco Ladeira <mauricioafl001@gmail.com>
2022-04-25 19:22:10 -0300

    projeto lp3 utilizando springboot

---
+++ src/main/java/com/lp3/projeto/Lp3apiApplication.java
@@ -0,0 +1,13 @@
+package com.lp3.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lp3apiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lp3apiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lp3/projeto/Lp3apiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lp3.projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lp3apiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
