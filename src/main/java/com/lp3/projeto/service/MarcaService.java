fdf08c50a271121b538650abf208a377fa92198d
Mauricio Azevedo Franco Ladeira <mauricioafl001@gmail.com>
2022-05-24 20:27:16 -0300

    argumentos adicionados a classe MarcaService e ProdutoService

--- src/main/java/com/lp3/projeto/service/MarcaService.java
+++ src/main/java/com/lp3/projeto/service/MarcaService.java
@@ -1,7 +1,45 @@
 package com.lp3.projeto.service;
 
+import com.lp3.projeto.model.entity.Marca;
+import com.lp3.projeto.model.repository.MarcaRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class MarcaService {
+
+    private MarcaRepository repository;
+
+    public MarcaService(MarcaRepository repository){
+        this.repository=repository;
+    }
+
+    public List<Marca> getMarcas() {
+        return repository.findAll();
+    }
+
+    public Optional<Marca> getMarcaById(Long id) {
+        return repository.findById(id);
+    }
+
+    @Transactional
+    public Marca salvar(Marca marca) {
+        validar(marca);
+        return repository.save(marca);
+    }
+
+    @Transactional
+    public void excluir(Marca marca) {
+        Objects.requireNonNull(marca.getId());
+        repository.delete(marca);
+    }
+
+    public void validar(Marca marca) {
+
+    }
+
 }
--- src/main/java/com/lp3/projeto/service/ProdutoService.java
+++ src/main/java/com/lp3/projeto/service/ProdutoService.java
@@ -1,10 +1,13 @@
 package com.lp3.projeto.service;
 
+import com.lp3.projeto.exception.RegraNegocioException;
 import com.lp3.projeto.model.entity.Produto;
 import com.lp3.projeto.model.repository.ProdutoRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -24,5 +27,19 @@ public class ProdutoService {
         return repository.findById(id);
     }
 
+    @Transactional
+    public Produto salvar(Produto produto){
+        validar(produto);
+        return repository.save(produto);
+    }
 
+    @Transactional
+    public void excluir(Produto produto){
+        Objects.requireNonNull(produto.getId());
+        repository.delete(produto);
+    }
+
+    public void validar(Produto produto) {
+
+    }
 }
