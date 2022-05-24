5e8bba13374c48295e7a408f59ada2b0b8f531ce
Mauricio Azevedo Franco Ladeira <mauricioafl001@gmail.com>
2022-05-24 20:37:44 -0300

    argumentos adicionados a classe FornecedorService e DescontoService

--- src/main/java/com/lp3/projeto/service/DescontoService.java
+++ src/main/java/com/lp3/projeto/service/DescontoService.java
@@ -1,7 +1,43 @@
 package com.lp3.projeto.service;
 
+import com.lp3.projeto.model.entity.Desconto;
+import com.lp3.projeto.model.repository.DescontoRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Objects;
 
 @Service
 public class DescontoService {
+
+    private DescontoRepository repository;
+
+    public DescontoService(DescontoRepository repository){
+        this.repository=repository;
+    }
+
+    public List<Desconto> getDesconto(){
+        return repository.findAll();
+    }
+
+    public Desconto getDescontoById(Long id){
+        return repository.getById(id);
+    }
+
+    @Transactional
+    public Desconto salvar(Desconto desconto){
+        validar(desconto);
+        return repository.save(desconto);
+    }
+
+    @Transactional
+    public void excluir(Desconto desconto){
+        Objects.requireNonNull(desconto.getId());
+        repository.delete(desconto);
+    }
+
+    public void validar(Desconto desconto) {
+
+    }
 }
--- src/main/java/com/lp3/projeto/service/FornecedorService.java
+++ src/main/java/com/lp3/projeto/service/FornecedorService.java
@@ -1,7 +1,44 @@
 package com.lp3.projeto.service;
 
+import com.lp3.projeto.model.entity.Fornecedor;
+import com.lp3.projeto.model.repository.FornecedorRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class FornecedorService {
+
+    private FornecedorRepository repository;
+
+    public FornecedorService(FornecedorRepository repository){
+        this.repository=repository;
+    }
+
+    public List<Fornecedor> getFornecedor(){
+        return repository.findAll();
+    }
+
+    public Optional<Fornecedor> getFornecdorById(Long id){
+        return repository.findById(id);
+    }
+
+    @Transactional
+    public Fornecedor salvar(Fornecedor fornecedor){
+        validar(fornecedor);
+        return repository.save(fornecedor);
+    }
+
+    @Transactional
+    public void excluir(Fornecedor fornecedor){
+        Objects.requireNonNull(fornecedor.getId());
+        repository.delete(fornecedor);
+    }
+
+    public void validar(Fornecedor fornecedor) {
+
+    }
 }
