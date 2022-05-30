d40f1aafa5e5e4e63e242e6f896cadc82a230404
Mauricio Azevedo Franco Ladeira <mauricioafl001@gmail.com>
2022-05-30 15:08:41 -0300

    Criação da CompraService

--- src/main/java/com/lp3/projeto/service/CompraService.java
+++ src/main/java/com/lp3/projeto/service/CompraService.java
@@ -1,7 +1,44 @@
 package com.lp3.projeto.service;
 
+import com.lp3.projeto.model.entity.Compra;
+import com.lp3.projeto.model.repository.CompraRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class CompraService {
+
+    private CompraRepository repository;
+
+    public CompraService(CompraRepository repository){
+        this.repository=repository;
+    }
+
+    public List<Compra> getCompra(){
+        return repository.findAll();
+    }
+
+    public Optional<Compra> getCompraById(Long id){
+        return repository.findById(id);
+    }
+
+    @Transactional
+    public Compra salvar(Compra compra){
+        validar(compra);
+        return repository.save(compra);
+    }
+
+    @Transactional
+    public void excluir(Compra compra){
+        Objects.requireNonNull(compra);
+        repository.delete(compra);
+    }
+
+    public void validar(Compra compra){
+
+    }
 }
