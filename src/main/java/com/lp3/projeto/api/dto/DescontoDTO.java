15ea8a1ba290a9fbf88a664f5f1cd4dc29a7f903
Mauricio Azevedo Franco Ladeira <mauricioafl001@gmail.com>
2022-05-30 15:57:07 -0300

    Criado classe DTO Desconto

---
+++ src/main/java/com/lp3/projeto/api/dto/DescontoDTO.java
@@ -0,0 +1,28 @@
+package com.lp3.projeto.api.dto;
+
+import com.lp3.projeto.model.entity.Desconto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.modelmapper.ModelMapper;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DescontoDTO {
+
+    private Long id;
+    private String descricao;
+    private Date dataInicio;
+    private Date dataVencimento;
+    private Long idCategoria;
+
+    public static DescontoDTO create(Desconto desconto){
+        ModelMapper modelMapper = new ModelMapper();
+        return modelMapper.map(desconto, DescontoDTO.class);
+
+    }
+
+}
