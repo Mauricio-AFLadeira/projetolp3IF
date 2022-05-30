d61f90123afb7c4509acba55e09c7e5036ef19be
Mauricio Azevedo Franco Ladeira <mauricioafl001@gmail.com>
2022-05-30 15:13:09 -0300

    Criação da FormaPagamentoService

--- src/main/java/com/lp3/projeto/service/FormaPagamentoService.java
+++ src/main/java/com/lp3/projeto/service/FormaPagamentoService.java
@@ -1,7 +1,44 @@
 package com.lp3.projeto.service;
 
+import com.lp3.projeto.model.entity.FormaPagamento;
+import com.lp3.projeto.model.repository.FormaPagamentoRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class FormaPagamentoService {
+
+    private FormaPagamentoRepository repository;
+
+    public FormaPagamentoService (FormaPagamentoRepository repository){
+        this.repository=repository;
+    }
+
+    public List<FormaPagamento> getFormaDePagamento(){
+        return repository.findAll();
+    }
+
+    public Optional<FormaPagamento> getFormaDePagamentoById(Long id){
+        return repository.findById(id);
+    }
+
+    @Transactional
+    public FormaPagamento salvar(FormaPagamento formaPagamento){
+        validar(formaPagamento);
+        return repository.save(formaPagamento);
+    }
+
+    @Transactional
+    public void excluir(FormaPagamento formaPagamento){
+        Objects.requireNonNull(formaPagamento);
+        repository.delete(formaPagamento);
+    }
+
+    public void validar(FormaPagamento formaPagamento){
+
+    }
 }
