e1b8a7b389821d88de056d10e2d2cc92e7999b42
Mauricio Azevedo Franco Ladeira <mauricioafl001@gmail.com>
2022-05-30 16:24:54 -0300

    Criado classe DTO Produto

---
+++ src/main/java/com/lp3/projeto/api/dto/ProdutoDTO.java
@@ -0,0 +1,30 @@
+package com.lp3.projeto.api.dto;
+
+import com.lp3.projeto.model.entity.Produto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.modelmapper.ModelMapper;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProdutoDTO {
+
+    private Long id;
+    private String nome;
+    private String descricao;
+    private Integer valorUnitario;
+    private String imagem;
+    private String estoqueMax;
+    private String estoqueMin;
+    private String estoqueRessuprimento;
+    private String qtdeEstoque;
+    private Long idCategoria;
+    private Long idMarca;
+
+    public static ProdutoDTO create(Produto produto){
+        ModelMapper modelMapper = new ModelMapper();
+        return modelMapper.map(produto, ProdutoDTO.class);
+    }
+}
