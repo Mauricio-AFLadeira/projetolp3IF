4f4597c74888ade13097e7af4d258c19874cb0a7
Mauricio Azevedo Franco Ladeira <mauricioafl001@gmail.com>
2022-05-30 16:22:46 -0300

    Criado classe DTO Pedido

---
+++ src/main/java/com/lp3/projeto/api/dto/PedidoDTO.java
@@ -0,0 +1,24 @@
+package com.lp3.projeto.api.dto;
+
+import com.lp3.projeto.model.entity.Pedido;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.modelmapper.ModelMapper;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PedidoDTO {
+
+    private Long id;
+    private Date dataDoPedido;
+    private Long idPessoa;
+
+    public static PedidoDTO create(Pedido pedido){
+        ModelMapper modelMapper = new ModelMapper();
+        return modelMapper.map(pedido, PedidoDTO.class);
+    }
+}
