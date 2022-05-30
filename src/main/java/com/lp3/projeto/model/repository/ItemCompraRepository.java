36b5b3d5800fc9353b31106c051fe675e825d9b0
Mauricio Azevedo Franco Ladeira <mauricioafl001@gmail.com>
2022-05-30 15:42:05 -0300

    Criação dos Repositories e Services das classes ItemCompra e ItemPedido

---
+++ src/main/java/com/lp3/projeto/model/repository/ItemCompraRepository.java
@@ -0,0 +1,7 @@
+package com.lp3.projeto.model.repository;
+
+import com.lp3.projeto.model.entity.ItemCompra;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemCompraRepository extends JpaRepository<ItemCompra, Long> {
+}
