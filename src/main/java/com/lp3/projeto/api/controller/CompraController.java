b5670cfc0979afe6aa34aa46d14fef83ae5924b3
Patrick <patrickhauck0@gmail.com>
2022-06-07 12:19:01 -0300

    add Controllers Compra e Desconto

--- src/main/java/com/lp3/projeto/api/controller/CompraController.java
+++ src/main/java/com/lp3/projeto/api/controller/CompraController.java
@@ -1,4 +1,41 @@
 package com.lp3.projeto.api.controller;
 
+
+import com.lp3.projeto.api.dto.CompraDTO;
+import com.lp3.projeto.model.entity.Compra;
+import com.lp3.projeto.service.CompraService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/v1/compras")
+@RequiredArgsConstructor
 public class CompraController {
+
+        private final CompraService service;
+
+        @GetMapping()
+        public ResponseEntity get(){
+            List<Compra> compras = service.getCompra();
+            return ResponseEntity.ok(compras.stream().map(CompraDTO::create).collect(Collectors.toList()));
+        }
+
+        @GetMapping("/{id}")
+        public ResponseEntity get(@PathVariable("id") Long id){
+            Optional<Compra> compra = service.getCompraById(id);
+            if(!compra.isPresent()){
+                return new ResponseEntity("Compra não encontrada", HttpStatus.NOT_FOUND);
+            }
+
+            return ResponseEntity.ok(compra.map(CompraDTO::create));
+        }
 }
--- src/main/java/com/lp3/projeto/api/controller/DescontoController.java
+++ src/main/java/com/lp3/projeto/api/controller/DescontoController.java
@@ -1,4 +1,40 @@
 package com.lp3.projeto.api.controller;
 
+import com.lp3.projeto.api.dto.DescontoDTO;
+import com.lp3.projeto.model.entity.Desconto;
+import com.lp3.projeto.service.DescontoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/v1/descontos")
+@RequiredArgsConstructor
 public class DescontoController {
+
+    private final DescontoService service;
+
+    @GetMapping()
+    public ResponseEntity get(){
+        List<Desconto> descontos = service.getDesconto();
+        return ResponseEntity.ok(descontos.stream().map(DescontoDTO::create).collect(Collectors.toList()));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity get(@PathVariable("id") Long id){
+        Optional<Desconto> desconto = service.getDescontoById(id);
+        if(!desconto.isPresent()){
+            return new ResponseEntity("Desconto não encontrado", HttpStatus.NOT_FOUND);
+        }
+
+        return ResponseEntity.ok(desconto.map(DescontoDTO::create));
+    }
 }
