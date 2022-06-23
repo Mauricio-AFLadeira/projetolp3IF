c29e90f90e95616efcdbf8b37e61c7bcc425e0ef
Mauricio-AFLadeira <mauricioafl001@gmail.com>
2022-06-23 07:45:48 -0300

    add cruds in class Desconto

--- src/main/java/com/lp3/projeto/api/controller/DescontoController.java
+++ src/main/java/com/lp3/projeto/api/controller/DescontoController.java
@@ -1,15 +1,14 @@
 package com.lp3.projeto.api.controller;
 
 import com.lp3.projeto.api.dto.DescontoDTO;
+import com.lp3.projeto.exception.RegraNegocioException;
 import com.lp3.projeto.model.entity.Desconto;
 import com.lp3.projeto.service.DescontoService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -36,5 +35,50 @@ public class DescontoController {
         }
 
         return ResponseEntity.ok(desconto.map(DescontoDTO::create));
+    }
+
+    @PostMapping()
+    public ResponseEntity post(DescontoDTO dto) {
+        try {
+            Desconto desconto = converter(dto);
+            desconto= service.salvar(desconto);
+            return new ResponseEntity(desconto, HttpStatus.CREATED);
+        } catch (RegraNegocioException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity put(@PathVariable("id") Long id, DescontoDTO dto) {
+        if (!service.getDescontoById(id).isPresent()) {
+            return new ResponseEntity("Desconto não encontrado", HttpStatus.NOT_FOUND);
+        }
+        try {
+            Desconto desconto = converter(dto);
+            desconto.setId(id);
+            service.salvar(desconto);
+            return ResponseEntity.ok(desconto);
+        } catch (RegraNegocioException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity excluir(@PathVariable("id") Long id) {
+        Optional<Desconto> desconto = service.getDescontoById(id);
+        if (!desconto.isPresent()) {
+            return new ResponseEntity("Desconto não encontrado", HttpStatus.NOT_FOUND);
+        }
+        try {
+            service.excluir(desconto.get());
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        } catch (RegraNegocioException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    public Desconto converter(DescontoDTO dto) {
+        ModelMapper modelMapper = new ModelMapper();
+        return modelMapper.map(dto, Desconto.class);
     }
 }
